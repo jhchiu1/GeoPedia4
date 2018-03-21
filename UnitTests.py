a843019e4ef692592cfe9a7c5fa50582b8ce5a2d
JL <lollypopsandrainbows@gmail.com>
2018-03-20 19:23:11 -0500

    Tests twitterapi for valid date

--- UnitTests.py
+++ UnitTests.py
@@ -1,2 +1,8 @@
 from unittest import TestCase
-import query
+import twitterapi
+
+	def test_valid_date(self)
+	#tests if rice is a valid date and asserts the answer is no
+		twitterapi.valid_date('rice')
+		self.assertEqual("Not a valid date: '{0}'.")
+		self.assertRaises(argparse.ArgumentTypeError(msg))
